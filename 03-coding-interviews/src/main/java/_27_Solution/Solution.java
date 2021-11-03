8f8250882f874bac60eb917f1cf0cd9a6d66729e
yyq <leaf1013@outlook.com>
2021-11-04 01:18:48 +0800

    add solution

--- 03-coding-interviews/src/main/java/_27_Solution/Solution.java
+++ 03-coding-interviews/src/main/java/_27_Solution/Solution.java
@@ -1,7 +1,5 @@
 package _27_Solution;
 
-import java.util.Stack;
-
 /**
  * @author YeYaqiao
  * 剑指 Offer 27. 二叉树的镜像
@@ -9,21 +7,33 @@ import java.util.Stack;
 public class Solution {
 
     //栈解法
+//    public TreeNode mirrorTree(TreeNode root) {
+//        Stack<TreeNode> stack = new Stack<>();
+//        stack.add(root);
+//        while (!stack.isEmpty()) {
+//            TreeNode node = stack.pop();
+//            if (node == null)
+//                break;
+//            if (node.left != null)
+//                stack.add(node.left);
+//            if (node.right != null)
+//                stack.add(node.right);
+//            TreeNode temp = node.left;
+//            node.left = node.right;
+//            node.right = temp;
+//        }
+//        return root;
+//    }
+
+    //递归法
     public TreeNode mirrorTree(TreeNode root) {
-        Stack<TreeNode> stack = new Stack<>();
-        stack.add(root);
-        while (!stack.isEmpty()) {
-            TreeNode node = stack.pop();
-            if (node == null)
-                break;
-            if (node.left != null)
-                stack.add(node.left);
-            if (node.right != null)
-                stack.add(node.right);
-            TreeNode temp = node.left;
-            node.left = node.right;
-            node.right = temp;
-        }
+
+        if (root == null)
+            return null;
+        TreeNode leftNode = mirrorTree(root.right);
+        TreeNode rightNode = mirrorTree(root.left);
+        root.left = leftNode;
+        root.right = rightNode;
         return root;
     }
 }

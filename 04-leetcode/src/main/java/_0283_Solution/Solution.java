dc27f52069a4b71f3f288d326c02f10b1924aedf
yyq <leaf1013@outlook.com>
2021-11-09 01:32:54 +0800

    add solution

---
+++ 04-leetcode/src/main/java/_0283_Solution/Solution.java
@@ -0,0 +1,13 @@
+package _0283_Solution;
+
+/**
+ * @author YeYaqiao
+ * 283. 移动零
+ */
+public class Solution {
+    public void moveZeroes(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+
+        }
+    }
+}

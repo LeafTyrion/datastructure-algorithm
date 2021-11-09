0e563b09db5b8a59e7e2dee596d54b57831337c2
yeyaqiao <yyq9202021895@163.com>
2021-11-09 15:25:14 +0800

    add solution

--- 04-leetcode/src/main/java/_0189_Solution/Solution.java
+++ 04-leetcode/src/main/java/_0189_Solution/Solution.java
@@ -5,19 +5,34 @@ package _0189_Solution;
  * 189. 旋转数组
  */
 public class Solution {
+
+
+    //数组翻转3次
     public void rotate(int[] nums, int k) {
-        int n = nums.length;
-        k %= n;
-        reverse(nums, 0, n - 1);
-        reverse(nums, 0, k - 1);
-        reverse(nums, k, n - 1);
+        reverse(0, nums.length - 1, nums);
+        reverse(0, k % nums.length - 1, nums);
+        reverse(k % nums.length, nums.length - 1, nums);
     }
 
-    private void reverse(int[] nums, int start, int end) {
+    private void reverse(int start, int end, int[] nums) {
         while (start < end) {
             int temp = nums[start];
-            nums[start++] = nums[end];
-            nums[end--] = temp;
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
     }
+
+    //使用辅助数组
+//    public void rotate(int[] nums, int k) {
+//        int length = nums.length;
+//        int[] result = new int[length];
+//        k %= length;//取余简化
+//        for (int i = 0; i < length; i++) {
+//            result[(i + k) % length] = nums[i];
+//        }
+//        //将新数组内容赋值
+//        System.arraycopy(result, 0, nums, 0, length);
+//    }
 }

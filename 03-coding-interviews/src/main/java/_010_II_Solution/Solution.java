fbd91b81afb37e60167590da974be4b52a0f1c71
unknown <yeyaqiao@ESG.360ES.CN>
2021-11-16 16:37:29 +0800

    add solution

--- 03-coding-interviews/src/main/java/_010_II_Solution/Solution.java
+++ 03-coding-interviews/src/main/java/_010_II_Solution/Solution.java
@@ -6,6 +6,14 @@ package _010_II_Solution;
  */
 public class Solution {
     public int numWays(int n) {
-
+        if (n == 1 || n == 2)
+            return n;
+        int a = 1, b = 2, sum = 1;
+        for (int i = 3; i <= n; i++) {
+            sum = (a + b) % 1000000007;
+            a = b;
+            b = sum;
+        }
+        return sum;
     }
 }

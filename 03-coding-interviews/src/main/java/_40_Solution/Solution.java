288d620061a8651d5a213a035e1d2e72d7c55514
leaf <yaqiao_ye@foxmail.com>
2021-11-21 22:29:29 +0800

    add solution

---
+++ 03-coding-interviews/src/main/java/_40_Solution/Solution.java
@@ -0,0 +1,20 @@
+package _40_Solution;
+
+import java.util.Arrays;
+
+/**
+ * @author YeYaqiao
+ * 剑指 Offer 40. 最小的k个数
+ */
+public class Solution {
+
+    //利用 Arrays.sort() API
+//    public int[] getLeastNumbers(int[] arr, int k) {
+//        if (arr.length == 0)
+//            return new int[]{};
+//        Arrays.sort(arr);
+//        int[] result = new int[k];
+//        System.arraycopy(arr, 0, result, 0, k);
+//        return result;
+//    }
+}
--- 03-coding-interviews/src/main/java/_61_Solution/Solution.java
+++ 03-coding-interviews/src/main/java/_61_Solution/Solution.java
@@ -1,11 +1,43 @@
 package _61_Solution;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author YeYaqiao
  * 剑指 Offer 61. 扑克牌中的顺子
  */
 public class Solution {
-    public boolean isStraight(int[] nums) {
 
+    //利用排序 + 遍历，两个元素之间肯定不相等
+    public boolean isStraight(int[] nums) {
+        Arrays.sort(nums);
+        int joker = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == 0)
+                joker++;
+            else if (nums[i] == nums[i + 1])
+                return false;
+        }
+        return nums[nums.length - 1] - nums[joker] < 5;
     }
+
+    //利用 hashset
+    //顺子中除0以外没有重复的牌，并且最大值减最小值不能超过5
+//    public boolean isStraight(int[] nums) {
+//        int min = 0;
+//        int max = 0;
+//        Set<Integer> set = new HashSet<>();
+//        for (int num : nums) {
+//            if (num == 0)
+//                continue;
+//            max = Math.max(num, max);
+//            min = Math.min(num, min);
+//            if (set.contains(num))
+//                return false;
+//            set.add(num);
+//        }
+//        return max - min < 5;
+//    }
 }

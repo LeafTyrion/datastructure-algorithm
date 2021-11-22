6430e6cd873980b28d9507d161dfa6a928ab0fb7
yyq <leaf1013@outlook.com>
2021-11-23 01:18:48 +0800

    add solution

--- 03-coding-interviews/src/main/java/_55_II_Solution/Solution.java
+++ 03-coding-interviews/src/main/java/_55_II_Solution/Solution.java
@@ -7,16 +7,13 @@ package _55_II_Solution;
 public class Solution {
 
     public boolean isBalanced(TreeNode root) {
-        return recur(root) != -1;
+        if (root == null) return true;
+        return Math.abs(depth(root.left) - depth(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
     }
 
-    private int recur(TreeNode root) {
+    private int depth(TreeNode root) {
         if (root == null) return 0;
-        int left = recur(root.left);
-        if(left == -1) return -1;
-        int right = recur(root.right);
-        if(right == -1) return -1;
-        return Math.abs(left - right) < 2 ? Math.max(left, right) + 1 : -1;
+        return Math.max(depth(root.left), depth(root.right)) + 1;
     }
 }
 

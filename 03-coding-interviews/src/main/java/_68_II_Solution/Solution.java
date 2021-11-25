5d55b99e7fb7a9ef1059a3e2744d972968a8efc7
leaf <yaqiao_ye@foxmail.com>
2021-11-25 19:37:17 +0800

    add solution

--- 03-coding-interviews/src/main/java/_68_II_Solution/Solution.java
+++ 03-coding-interviews/src/main/java/_68_II_Solution/Solution.java
@@ -6,7 +6,16 @@ package _68_II_Solution;
  */
 public class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-
+        //若当前节点为空获取是其中任意一个子节点时，返回root即可
+        if(root == null || root == p || root == q) return root;
+        //遍历左子节点
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        //遍历右子节点
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        //当左节点为空，返回右节点
+        if(left == null) return right;
+        if(right == null) return left;
+        return root;
     }
 }
 

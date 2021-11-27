3a7c921090a5b9ad40a7ad6f9e3011fe7ab2a1a1
yyq <yyq920201895@163.com>
2021-11-27 23:46:07 +0800

    add solution

--- 03-coding-interviews/src/main/java/_68_II_Solution/Solution.java
+++ 03-coding-interviews/src/main/java/_68_II_Solution/Solution.java
@@ -5,16 +5,35 @@ package _68_II_Solution;
  * 剑指 Offer 68 - II. 二叉树的最近公共祖先
  */
 public class Solution {
+
+
+    //递归法
+    //二叉树的最近公共祖先的条件：
+    //p、q 分别在root的两侧，或者p、q在root的一侧，并且root为p或者q
+    //使用前序遍历，搜索整个二叉树
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        //若当前节点为空获取是其中任意一个子节点时，返回root即可
-        if(root == null || root == p || root == q) return root;
-        //遍历左子节点
+        //向下递归时做的操作
+        //若当前节点为null，则证明以及越过叶子节点，并且没有找到目标p，q，则向上返回null
+        if (root == null)
+            return null;
+        //若当前节点为p，则说明找到了p节点，向上返回当前节点p
+        if (root == p)
+            return root;
+        //若当前节点为q，则说明找到了q节点，向上返回当前节点q
+        if (root == q)
+            return root;
+        //遍历左子树操作，获取左子节点
         TreeNode left = lowestCommonAncestor(root.left, p, q);
-        //遍历右子节点
+        //遍历右子树操作，获取右子节点
         TreeNode right = lowestCommonAncestor(root.right, p, q);
-        //当左节点为空，返回右节点
-        if(left == null) return right;
-        if(right == null) return left;
+        //回溯时做的操作
+        //若左子节点为空，则说明左子树没有目标节点，则返回右子节点
+        if (left == null)
+            return right;
+        //若右子节点为空，则说明右子树没有目标节点，则返回左子节点
+        if (right == null)
+            return left;
+        //若左右节点都不为空，则当前节点为公共父节点
         return root;
     }
 }

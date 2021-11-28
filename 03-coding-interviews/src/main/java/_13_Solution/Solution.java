b190d612973046bd082f13e7f4462428f9ed0e18
yyq <yyq920201895@163.com>
2021-11-29 00:57:00 +0800

    add solution

--- 03-coding-interviews/src/main/java/_13_Solution/Solution.java
+++ 03-coding-interviews/src/main/java/_13_Solution/Solution.java
@@ -5,7 +5,32 @@ package _13_Solution;
  * 剑指 Offer 13. 机器人的运动范围
  */
 public class Solution {
-    public int movingCount(int m, int n, int k) {
+    //dfs 深度优先搜索
+    private int m, n, k;
+    private boolean visited[][];
 
+    public int movingCount(int m, int n, int k) {
+        this.m = m;
+        this.n = n;
+        this.k = k;
+        this.visited = new boolean[m][n];
+        //从矩阵的左上角开始递归遍历，进行dfs
+        return dfs(0, 0, 0, 0);
+    }
+
+    private int dfs(int i, int j, int si, int sj) {
+        //若要访问的当前元素已经越界、位数之和超过了k或者已经被访问过，则表示没有可以走的路，返回0
+        if (i >= m || j >= n || k < si + sj || visited[i][j])
+            return 0;
+        //若当前元素符合访问要求，则将存入 visited 中，标记成已访问
+        visited[i][j] = true;
+        //继续递归判断当前元素的周围元素是否符合要求
+        //计算要递归的下一个元素的横坐标位数之和和纵坐标位数之和
+        //获取当前列下一行元素的位数之和
+        int sumI = (i + 1) % 10 != 0 ? si + 1 : si - 8;
+        //获取当前行下一列元素的位数之和
+        int sumJ = (j + 1) % 10 != 0 ? sj + 1 : sj - 8;
+        //递归并获取结果+1
+        return 1 + dfs(i + 1, j, sumI, sj) + dfs(i, j + 1, si, sumJ);
     }
 }

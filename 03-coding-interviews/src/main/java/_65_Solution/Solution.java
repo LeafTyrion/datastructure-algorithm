0483d778d8623f54adf1706a64f6ff0950a54474
leaf <yaqiao_ye@foxmail.com>
2021-11-29 21:39:55 +0800

    add solution

--- 03-coding-interviews/src/main/java/_65_Solution/Solution.java
+++ 03-coding-interviews/src/main/java/_65_Solution/Solution.java
@@ -6,6 +6,14 @@ package _65_Solution;
  */
 public class Solution {
     public int add(int a, int b) {
-
+        while (a != 0) {
+            //计算进位的值
+            int temp = (a & b) << 1;
+            //计算当前位的值
+            b = b ^ a;
+            //获取进位值，若不为0，则继续计算
+            a = temp;
+        }
+        return b;
     }
 }

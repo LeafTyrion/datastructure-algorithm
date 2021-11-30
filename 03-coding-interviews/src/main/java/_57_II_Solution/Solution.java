57fb89627c8fdbb3f23d4b26839b563c16994fb7
leaf <yaqiao_ye@foxmail.com>
2021-11-30 22:03:00 +0800

    add solution

--- 03-coding-interviews/src/main/java/_57_II_Solution/Solution.java
+++ 03-coding-interviews/src/main/java/_57_II_Solution/Solution.java
@@ -1,13 +1,70 @@
 package _57_II_Solution;
 
-import java.util.Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author YeYaqiao
  * 剑指 Offer 57 - II. 和为s的连续正数序列
  */
 public class Solution {
+
+    //滑动窗口，双指针
     public int[][] findContinuousSequence(int target) {
-        Arrays.sort();
+        List<int[]> list = new ArrayList<>();
+        int sum = 0;
+        int l = 1;
+        for (int r = 1; r < target; r++) {
+            sum += r;
+            while (sum > target) {
+                sum -= l++;
+            }
+            if (sum == target) {
+                int[] temp = new int[r - l + 1];
+                for (int i = 0; i < temp.length; i++) {
+                    temp[i] = l + i;
+                }
+                list.add(temp);
+            }
+        }
+
+        int[][] res = new int[list.size()][];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+
+
+    //暴力优化
+//    public int[][] findContinuousSequence(int target) {
+//        List<int[]> result = new ArrayList<>();
+//        List<Integer> temp = new ArrayList<>();
+//
+//        for (int i = 1; i <= target / 2; i++) {
+//            int next = i + 1;
+//            int sum = i;
+//            temp.clear();
+//            temp.add(i);
+//            while (next < target && sum < target) {
+//                sum += next;
+//                temp.add(next);
+//                if (sum == target) {
+//                    int[] array = new int[temp.size()];
+//                    for (int i1 = 0; i1 < temp.size(); i1++) {
+//                        array[i1] = temp.get(i1);
+//                    }
+//                    result.add(array);
+//                }
+//                next++;
+//            }
+//        }
+//        return result.toArray(new int[0][]);
+//    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.findContinuousSequence(5);
     }
 }

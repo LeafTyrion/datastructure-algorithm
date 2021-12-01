5709bee4557f3d77186151b0854abac0bf61e049
yyq <leaf1013@outlook.com>
2021-12-02 01:55:21 +0800

    add solution

--- 03-coding-interviews/src/main/java/_07_Solution/Solution.java
+++ 03-coding-interviews/src/main/java/_07_Solution/Solution.java
@@ -15,7 +15,7 @@ public class Solution {
     //中序遍历可以根据根节点区分去左子树和右子树
     //保存前序遍历以便确定根节点
     int[] preorder;
-    //映射中序遍历以便获取左,右子树
+    //映射中序遍历以便通过根节点获取左,右子树
     Map<Integer, Integer> map = new HashMap<>();
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
@@ -25,12 +25,13 @@ public class Solution {
         }
 
         //递归入口,传入当前根节点索引(从前序遍历获取)
-        //左子节点索引和右子节点索引(从中序遍历获取)
+        //当前树的根节点索引，左边界索引，右边界索引
         return recur(0, 0, preorder.length - 1);
     }
 
     //递归当前节点分别获取和构建其左节点和右节点
     private TreeNode recur(int rootIndex, int leftIndex, int rightIndex) {
+        //当前树左子树的边界=右子树的边界，则表示当前节点为叶子节点，则无需为其构建左右节点
         if (leftIndex > rightIndex)
             return null;
 
@@ -40,9 +41,9 @@ public class Solution {
         //在中序遍历中获取左子树和右子树的分隔点
         int partition = map.get(preorder[rootIndex]);
 
-        int nextRoot=rootIndex+1;
-
-        //构建当前节点的左子节点和右子节点
+        //在先序遍历中，当前树的根节点索引+1，则是它左子树的根节点的索引
+        //partition - leftIndex + 1 表示左子树的长度
+        //先序遍历中的根节点索引+左子树的长度=右子树的根节点索引
         node.left = recur(rootIndex + 1, leftIndex, partition - 1);
         node.right = recur(rootIndex + partition - leftIndex + 1, partition + 1, rightIndex);
 
@@ -52,7 +53,7 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.buildTree(new int[]{9, 4, 7, 8, 6}, new int[]{7, 4, 8, 9, 6});
+        solution.buildTree(new int[]{3, 9, 20, 15, 7}, new int[]{9, 3, 15, 20, 7});
     }
 }
 

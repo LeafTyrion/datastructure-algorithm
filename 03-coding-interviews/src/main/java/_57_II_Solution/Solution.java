ffe4c926ee9320bfaa9f466b7c470c6eb231f3ac
leaf <yaqiao_ye@foxmail.com>
2021-12-01 15:49:37 +0800

    add solution

--- 03-coding-interviews/src/main/java/_57_II_Solution/Solution.java
+++ 03-coding-interviews/src/main/java/_57_II_Solution/Solution.java
@@ -10,41 +10,72 @@ import java.util.List;
  */
 public class Solution {
 
-    //滑动窗口，双指针
+    //滑动窗口,左闭右闭
     public int[][] findContinuousSequence(int target) {
         List<int[]> list = new ArrayList<>();
-        //窗口内元素的和
-        int sum = 0;
-        //初始化左右指针
+        int sum = 1;
         int left = 1, right = 1;
-        while (right <= target) {
+        while (right < target) {
             if (sum < target) {
-                //当窗口内元素的和小于target时，
-                //左指针left不动，向右移动右指针right
-                //先计算当前窗口内的和
-                sum = sum + right;
                 right++;
+                sum = sum + right;
             } else if (sum > target) {
-                //当窗口内元素的和大于target时，
-                //右指针right不动，向右移动左指针left
-                //并减去左指针越过的值
                 sum = sum - left;
                 left++;
             } else {
-                int[] array = new int[right - left];
-                for (int k = left; k < right; k++) {
-                    array[k - left] = k;
+                int[] result = new int[right - left + 1];
+                int a = left;
+                for (int i = 0; i < right - left + 1; i++) {
+                    result[i] = a++;
                 }
-                list.add(array);
+                list.add(result);
                 sum = sum - left;
                 left++;
             }
         }
+
         int[][] result = new int[list.size()][];
         for (int i = 0; i < result.length; i++) {
             result[i] = list.get(i);
         }
         return result;
+
+
+//    //滑动窗口，双指针,左闭右开
+//    public int[][] findContinuousSequence(int target) {
+//        List<int[]> list = new ArrayList<>();
+//        //窗口内元素的和
+//        int sum = 0;
+//        //初始化左右指针
+//        int left = 1, right = 1;
+//        while (right <= target) {
+//            if (sum < target) {
+//                //当窗口内元素的和小于target时，
+//                //左指针left不动，向右移动右指针right
+//                //先计算当前窗口内的和
+//                sum = sum + right;
+//                right++;
+//            } else if (sum > target) {
+//                //当窗口内元素的和大于target时，
+//                //右指针right不动，向右移动左指针left
+//                //并减去左指针越过的值
+//                sum = sum - left;
+//                left++;
+//            } else {
+//                int[] array = new int[right - left];
+//                for (int k = left; k < right; k++) {
+//                    array[k - left] = k;
+//                }
+//                list.add(array);
+//                sum = sum - left;
+//                left++;
+//            }
+//        }
+//        int[][] result = new int[list.size()][];
+//        for (int i = 0; i < result.length; i++) {
+//            result[i] = list.get(i);
+//        }
+//        return result;
 
 
 //        List<int[]> list = new ArrayList<>();
@@ -100,6 +131,6 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.findContinuousSequence(5);
+        solution.findContinuousSequence(9);
     }
 }

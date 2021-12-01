f87d4601002346678056f41cfb86f25e600d0671
leaf <yaqiao_ye@foxmail.com>
2021-12-01 12:39:33 +0800

    add solution

--- 03-coding-interviews/src/main/java/_57_II_Solution/Solution.java
+++ 03-coding-interviews/src/main/java/_57_II_Solution/Solution.java
@@ -13,27 +13,62 @@ public class Solution {
     //滑动窗口，双指针
     public int[][] findContinuousSequence(int target) {
         List<int[]> list = new ArrayList<>();
+        //窗口内元素的和
         int sum = 0;
-        int l = 1;
-        for (int r = 1; r < target; r++) {
-            sum += r;
-            while (sum > target) {
-                sum -= l++;
-            }
-            if (sum == target) {
-                int[] temp = new int[r - l + 1];
-                for (int i = 0; i < temp.length; i++) {
-                    temp[i] = l + i;
+        //初始化左右指针
+        int left = 1, right = 1;
+        while (right <= target) {
+            if (sum < target) {
+                //当窗口内元素的和小于target时，
+                //左指针left不动，向右移动右指针right
+                //先计算当前窗口内的和
+                sum = sum + right;
+                right++;
+            } else if (sum > target) {
+                //当窗口内元素的和大于target时，
+                //右指针right不动，向右移动左指针left
+                //并减去左指针越过的值
+                sum = sum - left;
+                left++;
+            } else {
+                int[] array = new int[right - left];
+                for (int k = left; k < right; k++) {
+                    array[k - left] = k;
                 }
-                list.add(temp);
+                list.add(array);
+                sum = sum - left;
+                left++;
             }
         }
-
-        int[][] res = new int[list.size()][];
-        for (int i = 0; i < res.length; i++) {
-            res[i] = list.get(i);
+        int[][] result = new int[list.size()][];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = list.get(i);
         }
-        return res;
+        return result;
+
+
+//        List<int[]> list = new ArrayList<>();
+//        int sum = 0;
+//        int l = 1;
+//        for (int r = 1; r < target; r++) {
+//            sum += r;
+//            while (sum > target) {
+//                sum -= l++;
+//            }
+//            if (sum == target) {
+//                int[] temp = new int[r - l + 1];
+//                for (int i = 0; i < temp.length; i++) {
+//                    temp[i] = l + i;
+//                }
+//                list.add(temp);
+//            }
+//        }
+//
+//        int[][] res = new int[list.size()][];
+//        for (int i = 0; i < res.length; i++) {
+//            res[i] = list.get(i);
+//        }
+//        return res;
     }
 
 

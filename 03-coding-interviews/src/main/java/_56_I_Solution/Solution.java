604f02f34333be595d17133a8a8b29474d45a3af
yyq <leaf1013@outlook.com>
2021-12-03 02:07:05 +0800

    add solution

---
+++ 03-coding-interviews/src/main/java/_56_I_Solution/Solution.java
@@ -0,0 +1,11 @@
+package _56_I_Solution;
+
+/**
+ * @author YeYaqiao
+ * 剑指 Offer 56 - I. 数组中数字出现的次数
+ */
+public class Solution {
+    public int[] singleNumbers(int[] nums) {
+
+    }
+}
---
+++ 03-coding-interviews/src/main/java/_62_Solution/Solution.java
@@ -0,0 +1,43 @@
+package _62_Solution;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author YeYaqiao
+ * 剑指 Offer 62. 圆圈中最后剩下的数字
+ */
+public class Solution {
+
+    //一共n个数字，每次都删除第m个数字
+    //约瑟夫环，动态规划
+    public int lastRemaining(int n, int m) {
+        //倒推法，当只剩一个数字时，他的位置索引为0
+        //所以他上一轮的位置为 (0+m)%数字的长度，之后的以此类推
+        int index = 0;
+        for (int i = 1; i <= n; i++) {
+            //i为当前数组的长度
+            index = (index + m) % i;
+        }
+        return index;
+    }
+
+    //模拟剔除
+//    public int lastRemaining(int n, int m) {
+//        List<Integer> temp = new ArrayList<>();
+//        for (int i = 0; i < n; i++) {
+//            temp.add(i);
+//        }
+//
+//        //从数组第一个元素开始寻找
+//        int index = 0;
+//        while (n > 1) {
+//            //假设当前要被删除的元素是index，则下一个要被删除的元素是 index+m，
+//            //由于当前位置被删除后，后面的元素会前移，实际被删除的元素为 index+m-1
+//            index = (index - 1 + m) % n;
+//            temp.remove(index);
+//            n--;
+//        }
+//        return temp.get(0);
+//    }
+}

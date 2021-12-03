505b0c0fd84b6d1f06600d50272d10be5afbadb7
leaf <yaqiao_ye@foxmail.com>
2021-12-03 18:58:53 +0800

    add solution

--- 03-coding-interviews/src/main/java/_56_II_Solution/Solution.java
+++ 03-coding-interviews/src/main/java/_56_II_Solution/Solution.java
@@ -1,11 +1,29 @@
 package _56_II_Solution;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author YeYaqiao
  * 剑指 Offer 56 - II. 数组中数字出现的次数 II
  */
 public class Solution {
-//    public int singleNumber(int[] nums) {
-//
-//    }
+
+    //暴力法
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            if (map.containsKey(num))
+                map.put(num, 2);
+            else
+                map.put(num, 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1)
+                return entry.getKey();
+        }
+        return 0;
+    }
 }

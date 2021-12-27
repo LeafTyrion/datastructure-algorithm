71e4794745414d6e6d33a8e0639440b0a6be079e
yyq <yyq920201895@163.com>
2021-12-28 00:52:24 +0800

    add solution

--- 04-leetcode/src/main/java/_0015_Solution/Solution.java
+++ 04-leetcode/src/main/java/_0015_Solution/Solution.java
@@ -1,5 +1,6 @@
 package _0015_Solution;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,6 +9,26 @@ import java.util.List;
  */
 public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (i == j || j == k || i == k)
+                        continue;
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        result.add(temp);
+                    }
+                }
+
+            }
+
+        }
+        return result;
     }
+
+
 }

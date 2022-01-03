477b6ab514e6a3ee30f7b7f149388c0bc462a0a7
yyq <yyq920201895@163.com>
2022-01-04 00:13:39 +0800

    add solution

--- 04-leetcode/src/main/java/_0022_Solution/Solution.java
+++ 04-leetcode/src/main/java/_0022_Solution/Solution.java
@@ -9,65 +9,63 @@ import java.util.List;
  */
 public class Solution {
 
-    //递归
+    //dfs+剪枝
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
-        generateAll(result, new StringBuilder(), 0, 0, n);
+        generateAll(n, result, "", 0, 0);
         return result;
     }
 
-    //用 n1,n2分别记录左括号和右括号的个数
-    private void generateAll(List<String> result, StringBuilder current, int n1, int n2, int n) {
-        //递归出口
-        if (n1 > n || n2 > n)
+    private void generateAll(int n, List<String> result, String current, int left, int right) {
+        //剪枝操作
+        //当左括号的的数量大于n或者右括号的数量比左括号多，则进行剪枝
+        if (right > left || left > n)
             return;
-
-        if (n1 == n && n2 == n)
-            result.add(new String(current));
-
-        if (n1 >= n2) {
-            StringBuilder temp = new StringBuilder(current);
-            //二叉树分支生成点
-            generateAll(result, current.append('('), n1 + 1, n2, n);
-            generateAll(result, temp.append(')'), n1, n2 + 1, n);
+        //递归完成回溯
+        if (current.length() == 2 * n) {
+            result.add(current);
+            return;
         }
-
+        //先添加左括号，再添加右括号
+        //由于java传递参数时是值传递，所以无需在回溯时对 current、left、right修改成原值
+        generateAll(n, result, current + "(", left + 1, right);
+        generateAll(n, result, current + ")", left, right + 1);
     }
 
+    public static void main(String[] args) {
+        System.out.println(new Solution().generateParenthesis(1));
+    }
 
+    //dfs 全排列
     //暴力法，递归生成所有括号组合，判断括号组合是否合法
 //    public List<String> generateParenthesis(int n) {
 //        List<String> result = new ArrayList<>();
-//        generateAll(new char[2 * n], 0, result);
+//        generateAll(n, result, "");
 //        return result;
 //    }
 //
-//    //递归生成所有可能得括号组合，每个位置都有两种可能
-//    private void generateAll(char[] current, int position, List<String> result) {
-//        if (position == current.length) {
+//    private void generateAll(int n, List<String> result, String current) {
+//        if (current.length() == 2 * n) {
 //            if (valid(current))
-//                result.add(new String(current));
-//        } else {
-//            current[position] = '(';
-//            generateAll(current, position + 1, result);
-//            current[position] = ')';
-//            generateAll(current, position + 1, result);
+//                result.add(current);
+//            return;
 //        }
+//        //先添加左括号，再添加右括号
+//        generateAll(n, result, current + "(");
+//        generateAll(n, result, current + ")");
 //    }
 //
-//    //判断一个括号组合是否合法
-//    //左括号和右括号在位置上一一对应
-//    private boolean valid(char[] current) {
+//    private boolean valid(String current) {
 //        int balance = 0;
-//        for (char c : current) {
+//        for (char c : current.toCharArray()) {
 //            if (c == '(')
 //                balance++;
 //            else
 //                balance--;
-//            //若一开始时 ) 则balance为负数，非法组合
 //            if (balance < 0)
 //                return false;
 //        }
 //        return balance == 0;
 //    }
+
 }

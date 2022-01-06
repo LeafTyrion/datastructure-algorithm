4b9f41caebbf99cb5a86569bed8494c7d19af53c
yyq <leaf1013@outlook.com>
2022-01-07 01:36:31 +0800

    add solution

--- 04-leetcode/src/main/java/_0094_Solution/Solution.java
+++ 04-leetcode/src/main/java/_0094_Solution/Solution.java
@@ -1,26 +1,44 @@
 package _0094_Solution;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author YeYaqiao
  * 94. 二叉树的中序遍历
  */
 public class Solution {
+
+    //BFS 广度优先搜索
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
-        dfs(result, root);
+        Deque<TreeNode> deque = new LinkedList<>();
+        while (root != null || !deque.isEmpty()) {
+            while (root != null) {
+                deque.push(root);
+                root = root.left;
+            }
+            root = deque.pop();
+            result.add(root.val);
+            root = root.right;
+        }
         return result;
     }
 
-    private void dfs(List<Integer> result, TreeNode root) {
-        if (root == null)
-            return;
-        dfs(result, root.left);
-        result.add(root.val);
-        dfs(result, root.right);
-    }
+
+    //DFS 递归深度优先搜索
+//    public List<Integer> inorderTraversal(TreeNode root) {
+//        List<Integer> result = new ArrayList<>();
+//        dfs(result, root);
+//        return result;
+//    }
+//
+//    private void dfs(List<Integer> result, TreeNode root) {
+//        if (root == null)
+//            return;
+//        dfs(result, root.left);
+//        result.add(root.val);
+//        dfs(result, root.right);
+//    }
 
 }
 

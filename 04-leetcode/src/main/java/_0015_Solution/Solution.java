a14a94b92771e179ba237cd175eda9da55b18adb
yyq <leaf1013@outlook.com>
2022-01-24 00:10:21 +0800

    add solution

--- 04-leetcode/src/main/java/_0015_Solution/Solution.java
+++ 04-leetcode/src/main/java/_0015_Solution/Solution.java
@@ -1,6 +1,7 @@
 package _0015_Solution;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -10,24 +11,41 @@ import java.util.List;
 public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        for (int i = 0; i < nums.length - 2; i++) {
-            for (int j = i + 1; j < nums.length - 1; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
-                    if (i == j || j == k || i == k)
-                        continue;
-                    if (nums[i] + nums[j] + nums[k] == 0) {
-                        List<Integer> temp = new ArrayList<>();
-                        temp.add(nums[i]);
-                        temp.add(nums[j]);
-                        temp.add(nums[k]);
-                        result.add(temp);
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0)
+                break;
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum > 0)
+                    right--;
+                else if (sum < 0)
+                    left++;
+                else {
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[left]);
+                    temp.add(nums[right]);
+                    result.add(temp);
+                    //0，1，2
+                    left++;
+                    while (left != right && nums[left - 1] == nums[left]) {
+                        left++;
                     }
+
                 }
-
             }
-
         }
         return result;
+    }
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
     }
 
 

fd237a84fb4c11454eb250a1888a16599ae08025
leaf <yaqiao_ye@foxmail.com>
2022-03-25 18:27:53 +0800

    init solution

---
+++ 04-leetcode/src/main/java/_0039_Solution/Solution.java
@@ -0,0 +1,13 @@
+package _0039_Solution;
+
+import java.util.List;
+
+/**
+ * @author YeYaqiao
+ * 39. 组合总和
+ */
+public class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+
+    }
+}

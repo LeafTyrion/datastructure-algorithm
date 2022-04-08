80502ad4615bf4559e1984a9abf95735fd84d2cf
leaf <yaqiao_ye@foxmail.com>
2022-04-08 18:38:06 +0800

    add wrong solution

--- 04-leetcode/src/main/java/_0039_Solution/Solution.java
+++ 04-leetcode/src/main/java/_0039_Solution/Solution.java
@@ -1,5 +1,6 @@
 package _0039_Solution;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,7 +8,33 @@ import java.util.List;
  * 39. 组合总和
  */
 public class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
 
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.combinationSum(new int[]{2, 3, 6, 7}, 7);
+    }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        dfs(result, candidates, target, 0, new ArrayList<>(candidates.length), 0);
+        return result;
+    }
+
+    private void dfs(List<List<Integer>> result, int[] candidates, int target, int sum, List<Integer> temp, int index) {
+
+        if()
+
+        for (int i = index; i < candidates.length; i++) {
+            int cur = candidates[i];
+            sum += cur;
+            if (sum == target) {
+                temp.add(cur);
+                result.add(new ArrayList<>(temp));
+                return;
+            } else if (sum <= target) {
+                if (index + 1 < candidates.length)
+                    dfs(result, candidates, target, sum, temp, index + 1);
+            }
+        }
     }
 }

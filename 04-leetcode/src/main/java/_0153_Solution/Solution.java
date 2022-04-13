e42562a817c77e7a0d94df757a308dbae9aae843
leaf <yaqiao_ye@foxmail.com>
2022-04-13 13:59:00 +0800

    add solution

--- 04-leetcode/src/main/java/_0153_Solution/Solution.java
+++ 04-leetcode/src/main/java/_0153_Solution/Solution.java
@@ -5,6 +5,7 @@ package _0153_Solution;
  * 153. 寻找旋转排序数组中的最小值
  */
 public class Solution {
+    //二分法
     public int findMin(int[] nums) {
         int length = nums.length;
         int left = 0;
@@ -18,4 +19,14 @@ public class Solution {
         }
         return nums[left];
     }
+
+    //暴力遍历
+//    public int findMin(int[] nums) {
+//        for (int i = 0; i < nums.length - 1; i++) {
+//            if (nums[i] > nums[i + 1])
+//                return nums[i + 1];
+//        }
+//        return -1;
+//    }
+
 }

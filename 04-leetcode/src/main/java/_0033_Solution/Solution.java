54e3bbcd1d0b14df2935f0fb747ba5a458483b3b
leaf <yaqiao_ye@foxmail.com>
2022-04-26 12:55:45 +0800

    add solution

--- 04-leetcode/src/main/java/_0033_Solution/Solution.java
+++ 04-leetcode/src/main/java/_0033_Solution/Solution.java
@@ -9,14 +9,24 @@ public class Solution {
 
         int left = 0;
         int right = nums.length - 1;
-        while (left < right) {
-            int middle = right - (right - left) / 2;
-           if(nums[middle]==target)
-               return middle;
-           if()
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            if (nums[middle] == target)
+                return middle;
+            if (nums[middle] < nums[right]) {
+                if (target > nums[middle] && target <= nums[right]) {
+                    left = middle + 1;
+                } else {
+                    right = middle - 1;
+                }
+            } else {
+                if (target >= nums[left] && target < nums[middle])
+                    right = middle - 1;
+                else
+                    left = middle + 1;
+            }
         }
 
         return -1;
-
     }
 }

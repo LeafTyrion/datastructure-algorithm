9c3e9d7e49b8aaefc8b5ab74180aa2f387f80d3b
leaf <920201895@qq.com>
2022-04-28 00:29:45 +0800

    add solution

---
+++ 04-leetcode/src/main/java/_0155_Solution/Solution.java
@@ -0,0 +1,34 @@
+package _0155_Solution;
+
+/**
+ * @author YeYaqiao
+ * 155. 最小栈
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
+
+class MinStack {
+
+    public MinStack() {
+
+    }
+
+    public void push(int val) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+
+    }
+
+    public int getMin() {
+
+    }
+}

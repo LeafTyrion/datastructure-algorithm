888e5b9c2826e06930c93ed788a75106772daaf7
leaf <yaqiao_ye@foxmail.com>
2022-04-27 15:24:36 +0800

    add solution

---
+++ 04-leetcode/src/main/java/_0141_Solution/Solution.java
@@ -0,0 +1,48 @@
+package _0141_Solution;
+
+/**
+ * @author YeYaqiao
+ * 141. 环形链表
+ */
+public class Solution {
+
+    //快慢指针
+    public boolean hasCycle(ListNode head) {
+        if (head == null)
+            return false;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (slow == null || fast == null || fast.next == null)
+                return false;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+
+
+    //hashset
+//    public boolean hasCycle(ListNode head) {
+//        Set<ListNode> set = new HashSet<>();
+//        while (head != null) {
+//            if (set.contains(head))
+//                return true;
+//            set.add(head);
+//            head = head.next;
+//        }
+//        return false;
+//    }
+
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+}

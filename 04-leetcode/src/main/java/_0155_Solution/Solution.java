ace14bca0e78f439c8c11dd1529ca15f4ad8bcdb
leaf <yaqiao_ye@foxmail.com>
2022-04-28 11:38:28 +0800

    add solution

--- 04-leetcode/src/main/java/_0155_Solution/Solution.java
+++ 04-leetcode/src/main/java/_0155_Solution/Solution.java
@@ -1,5 +1,8 @@
 package _0155_Solution;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author YeYaqiao
  * 155. 最小栈
@@ -12,23 +15,46 @@ public class Solution {
 
 class MinStack {
 
-    public MinStack() {
+    int min;
+    List<Integer> stack;
+    int pointer;
 
+    public MinStack() {
+        this.stack = new ArrayList<>();
+        this.pointer = -1;
     }
 
     public void push(int val) {
-
+        if (stack.isEmpty()) {
+            min = val;
+        } else {
+            if (val < min)
+                min = val;
+        }
+        pointer++;
+        stack.add(pointer, val);
     }
 
     public void pop() {
+        if (!stack.isEmpty()) {
+            Integer remove = stack.remove(pointer);
+            pointer--;
 
+            if (remove == min && pointer != -1) {
+                min = stack.get(pointer);
+                for (Integer integer : stack) {
+                    if (integer < min)
+                        min = integer;
+                }
+            }
+        }
     }
 
     public int top() {
-
+        return stack.get(pointer);
     }
 
     public int getMin() {
-
+        return min;
     }
 }

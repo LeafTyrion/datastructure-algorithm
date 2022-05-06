5fa01824b18079f6b7186f328e7718ac56c2f87b
leaf <920201895@qq.com>
2022-05-07 00:02:16 +0800

    add solution

---
+++ 04-leetcode/src/main/java/_0148_Solution/Solution.java
@@ -0,0 +1,28 @@
+package _0148_Solution;
+
+/**
+ * @author YeYaqiao
+ * 148. 排序链表
+ */
+public class Solution {
+    public ListNode sortList(ListNode head) {
+
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}

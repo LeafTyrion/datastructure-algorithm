40da467fd742d67114e88e741e7ecf78f81db4b1
leaf <920201895@qq.com>
2022-05-19 00:54:37 +0800

    add solution

--- 04-leetcode/src/main/java/_0215_Solution/Solution.java
+++ 04-leetcode/src/main/java/_0215_Solution/Solution.java
@@ -1,25 +1,43 @@
 package _0215_Solution;
 
-import java.util.Arrays;
-
 /**
  * @author YeYaqiao
  * 215. 数组中的第K个最大元素
  */
 public class Solution {
     public int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
-        int pointer=nums.length-1;
-        int count=1;
-        int tmp;
-        while (pointer>=0){
-            if(k==count)
-                return nums[pointer];
-            pointer--;
-            if(nums[pointer])
-                count++;
+        int length = nums.length;
+        quickSort(nums, 0, length - 1);
+        return nums[length - k];
+    }
+
+    private void quickSort(int[] nums, int left, int right) {
+        if (left < right) {
+            int partition = partition(nums, left, right);
+            quickSort(nums, left, partition - 1);
+            quickSort(nums, partition + 1, right);
         }
 
-        return 0;
     }
+
+    private int partition(int[] nums, int left, int right) {
+        int pivot = nums[left];
+        while (left < right) {
+            while (nums[right] >= pivot && left < right)
+                right--;
+            nums[left] = nums[right];
+            while (nums[left] <= pivot && left < right)
+                left++;
+            nums[right] = nums[left];
+        }
+        nums[left] = pivot;
+        return left;
+    }
+
+
+//    public int findKthLargest(int[] nums, int k) {
+//        Arrays.sort(nums);
+//        return nums[nums.length - k + 1];
+//    }
+
 }
